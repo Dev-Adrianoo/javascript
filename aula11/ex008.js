f2469cabdc96130095887e9de0f78fd21bb1c2c5
Adriano Souza <Adrianoshadow733@gmail.com>
2024-03-13 18:45:40 -0300

    exercício 10

---
+++ aula11/ex008.js
@@ -0,0 +1,5 @@
+var velocidade = 72
+console.log(`A velocidade do seu carro é ${velocidade}Km/h`);
+if (velocidade > 71) { // Condição Simples
+    console.log(`você ultrapassou a velocidade permitida, MULTADO!`);
+}
