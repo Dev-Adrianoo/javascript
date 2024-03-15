8492660401a2480cdf9147ff732b2fe82b692d99
Adriano Souza <Adrianoshadow733@gmail.com>
2024-03-14 22:43:01 -0300

    projeto da hora do dia

--- aula12ex/modelo/script.js
+++ aula12ex/modelo/script.js
@@ -2,17 +2,26 @@ function carregar(){
 var mensagem = window.document.getElementById('mensagem');
 var foto = window.document.getElementById('foto');
 var data = new Date();
-// var hora = data.getHours();
-var hora = 19
-mensagem.innerHTML = `Agora são ${hora} horas.`;
-if (hora >= 0 && hora <12) {
+var hora = data.getHours();
+var minutos = new Date()
+var minuto = minutos.getMinutes()
+// var hora = 19
+mensagem.innerHTML = `Agora são ${hora} horas e ${minuto} minutos.`;
+if (hora >= 0 && hora <=12) {
     //Bom dia!
-    foto.src = 'foto-manha'
-} else if (hora >12 && hora <18) {
+    imagem.setAttribute("src", "foto-manha.jpg"); /*sempre especifique o jpg, ou png. */
+    document.body.style.background = '#F0C8A9';
+} else if (hora >12 && hora <=18) {
     //Boa Tarde 
-    foto.src = 'foto-tarde'
+    imagem.setAttribute("src", "foto-tarde.jpg")
+    document.body.style.background = '#EE7F09'
 } else {
     //Boa noite
-    
+    imagem.setAttribute("src", "foto-noite.jpg")
+    document.body.style.background = '#1D3C4B'
     }
 }
+
+
+// obs setAttribute serve pra setar 2 atributos.
+//document.body.style.background = " " 
