7b35ab6e46653b7120cca58f957b55fac3eac81b
Adriano Souza <Adrianoshadow733@gmail.com>
2024-03-20 10:24:44 -0300

    Mais exercícios.

--- aula17/function04.js
+++ aula17/function04.js
@@ -1,9 +1,22 @@
-function fatorial (n) {
-    let fat = 1
-    for(let c = n; c > 1; c--) {
-        fat *= c
+// function fatorial (n) {
+//     let fat = 1
+//     for(let c = n; c > 1; c--) {
+//         fat *= c
+//     }
+//     return fat 
+// }
+
+// console.log(fatorial(5))
+
+//Recursividade
+function fatorial(n) {
+    if(n == 1) {
+        return 1
+    } else {
+        return n * fatorial(n - 1);
     }
-    return fat 
 }
 
-console.log(fatorial(5))
+console.log(fatorial(4))
+
+// 5 * 4 * 3 * 2 * 1
